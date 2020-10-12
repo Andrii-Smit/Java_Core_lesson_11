25c5e407c35e5f2c6eb003e3d6a712f2536c626f
lovak_000 <lovak_000@Koval>
2020-10-12 21:09:28 +0300

    added new projeck to github

---
+++ homework/src/task2/Engine.java
@@ -0,0 +1,19 @@
+package task2;
+
+public class Engine {
+
+	private int numberOfCylinders;
+
+	public Engine(int numberOfCylinders) {
+		this.numberOfCylinders = numberOfCylinders;
+	}
+
+	public int getNumberOfCylinders() {
+		return numberOfCylinders;
+	}
+
+	@Override
+	public String toString() {
+		return "Engine [numberOfCylinders=" + numberOfCylinders + "]";
+	}
+}
